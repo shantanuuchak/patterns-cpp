1c66b191bcc0fa8f053758066b5a9672548c4f22
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-18 11:28:19 +0000

    sum program

---
+++ 05-basic-programs/01-multiply-nums.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1;
+    int num2;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << endl;
+    cout << "Enter second number: ";
+    cin >> num2;
+    
+    // calculating sum
+    int sum = num1 + num2;
+    cout << "The sum is: " << sum << endl;
+}
