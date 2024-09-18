ff106deb1e05090cd2c70d0f70f510f68b71e690
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-18 11:32:03 +0000

    find perimeter

---
+++ 05-basic-programs/02-find-△-perimeter.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Enter first side: ";
+    int side1;
+    cin >> side1;
+    cout << endl;
+
+    cout << "Enter second side: ";
+    int side2;
+    cin >> side2;
+    cout << endl;
+
+    cout << "Enter third side: ";
+    int side3;
+    cin >> side3;
+    cout << endl;
+
+    int perimeter = side1 + side2 + side3;
+    cout << "The perimeter is: " << perimeter;
+} 
