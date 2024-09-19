e819335a94887702b19bf220ff05f79ab36bec11
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-19 10:34:51 +0000

    fixing logic for valid triangle

---
+++ 05-basic-programs/07-check-valid-triangle-▲.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int side1;
+    int side2;
+    int side3;
+
+    cout << "Enter first side: ";
+    cin >> side1;
+
+    cout << "Enter second side: ";
+    cin >> side2;
+
+    cout << "Enter third side: ";
+    cin >> side3;
+
+    bool isValid = true;
+    if(side1 + side2 <= side3){
+        isValid = false;
+    }
+    if (side2 + side3 <= side1){
+        isValid = false;
+    }
+    if(side3 + side1 <= side2){
+        isValid = false;
+    }
+
+    if(isValid){
+        cout << "This is a valid triangle ▲" << endl;
+    } else {
+        cout << "Not a valid triangle ▲" << endl;
+    }
+}
