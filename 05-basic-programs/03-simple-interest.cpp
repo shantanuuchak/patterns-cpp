8298835a0b1c62cb7e8473d635d7f197ada40cfc
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-19 10:16:12 +0000

    simple interest program

---
+++ 05-basic-programs/03-simple-interest.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Enter principal amount: ";
+    int p;
+    cin >> p;
+
+    cout << "Enter rate of interest: ";
+    int r;
+    cin >> r;
+
+    cout << "Enter time period: ";
+    int t;
+    cin >> t;
+
+    int i = (p*r*t)/100;
+    cout << "SI is: " << i << endl;
+}
