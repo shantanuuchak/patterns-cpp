e3c4d8b94036a9e3aa23b76b7e73be7cf95115c5
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-19 10:20:59 +0000

    find factorial code

---
+++ 05-basic-programs/05-find-factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Enter number to find factorial: ";
+    int n;
+    cin >> n;
+    
+    // Factorial
+    int factorial = 1;
+    for(int i=0; i<n; i++){
+        factorial *= i+1;
+    }
+    cout << "Factorial is: " << factorial << endl;
+    return factorial;
+}
