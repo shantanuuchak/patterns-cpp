d8fe690b60fd365e28c9866a146a805ed8f0d954
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-21 06:06:14 +0000

    pyramid pattern

--- 05-patterns/08-inverted-full-pyramid.cpp
+++ 05-patterns/08-inverted-full-pyramid.cpp
@@ -1,17 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     cout << "Enter a number: ";
     int n;
     cin >> n;
 
-    for(int i=0; i<n; i++){
-        for(int j=0; j<i; j++){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
             cout << "  ";
         }
 
-        for(int j=0; j<n-i; j++){
+        for (int j = 0; j < n - i; j++)
+        {
             cout << "*   ";
         }
 
