c2e1fdfbcc545af02245eea46ff5916e1197cdf9
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2024-09-21 06:14:07 +0000

    setting the stage

---
+++ 05-patterns/10-hollow-full-pyramid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter length: ";
+    int n;
+    cin >> n;
+    for (int row = 0; row < n; row++)
+    {
+        // Empty pyramid
+        // Star pyramid
+        // Empty pyramid
+    }
+}
